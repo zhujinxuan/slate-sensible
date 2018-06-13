a6dc90884a514b1ee7270fc2a7bdf3719f4f6a7a
zhujinxuan <zhujinxuan@gmail.com>
2018-06-13 10:29:06 -0400

    Update rollup config

--- support/rollup/config.js
+++ support/rollup/config.js
@@ -1,7 +1,12 @@
 import factory from './factory';
 import slateOnSelect from '../../packages/slate-on-select/package.json';
 import slateLineDelete from '../../packages/slate-delete-one-line/package.json';
+import slatePoorMentions from '../../packages/slate-poor-mentions/package.json';
 
-const configurations = [...factory(slateOnSelect), ...factory(slateLineDelete)];
+const configurations = [
+    ...factory(slateOnSelect),
+    ...factory(slateLineDelete),
+    ...factory(slatePoorMentions)
+];
 
 export default configurations;
