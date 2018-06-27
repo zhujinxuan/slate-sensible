85b11f8a90c9f632c28612fada6658af2b0d3154
zhujinxuan <zhujinxuan@gmail.com>
2018-06-27 15:16:46 -0400

    Remove enter keys

--- packages/slate-text-table/src/handlers/index.js
+++ packages/slate-text-table/src/handlers/index.js
@@ -1,8 +1,7 @@
-import onEnter from './onEnter';
 import onModEnter from './onModEnter';
 import onTab from './onTab';
 import onBackspace from './onBackspace';
 import onUpDown from './onUpDown';
 import onKeyDown from './onKeyDown';
 
-export { onEnter, onModEnter, onTab, onBackspace, onUpDown, onKeyDown };
+export { onModEnter, onTab, onBackspace, onUpDown, onKeyDown };
