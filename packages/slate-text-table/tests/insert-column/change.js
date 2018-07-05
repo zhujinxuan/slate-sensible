06b18f536e42e79abdc853459ac83f4bd60fbb03
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 16:07:40 -0400

    Fix hyperscript for insert-col

--- packages/slate-text-table/tests/insert-column/change.js
+++ packages/slate-text-table/tests/insert-column/change.js
@@ -1,7 +1,56 @@
-export default function(plugin, change) {
-    const { value } = change;
-    const cursorBlock = value.document.getDescendant('_cursor_');
-    change.moveToRangeOf(cursorBlock);
+/** @jsx h */
 
+import h from '../h';
+
+export default function(plugin, change) {
     return plugin.changes.insertColumn(change);
 }
+
+export const input = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">
+                        <anchor />Col 1, Row 1<focus />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left" />
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">Col 1, Row 1</td>
+                    <td textAlign="left">
+                        <cursor />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left" />
+                </tr>
+            </table>
+        </document>
+    </value>
+);
