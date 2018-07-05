1806311bb1a776d133a573cdb5a6b04e90f9b391
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 17:18:39 -0400

    Remove column

--- packages/slate-text-table/tests/remove-column-last-one/change.js
+++ packages/slate-text-table/tests/remove-column-last-one/change.js
@@ -1,7 +1,56 @@
-export default function(plugin, change) {
-    const { value } = change;
-    const cursorBlock = value.document.getDescendant('_cursor_');
-    change.moveToRangeOf(cursorBlock);
+/** @jsx h */
 
+import h from '../h';
+
+export default function(plugin, change) {
     return plugin.changes.removeColumn(change);
 }
+
+export const input = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left" />
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">Col 1, Row 1</td>
+                    <td textAlign="left">
+                        <cursor />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left" />
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">
+                        Col 1, Row 1<cursor />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
