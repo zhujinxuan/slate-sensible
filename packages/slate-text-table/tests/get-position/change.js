8bb2d56ebebceb4598d8dc38be83f3aa20a66853
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 15:57:59 -0400

    Fix styling

--- packages/slate-text-table/tests/get-position/change.js
+++ packages/slate-text-table/tests/get-position/change.js
@@ -1,18 +1,43 @@
-import expect from 'expect';
+/** @jsx h */
+
+import h from '../h';
 
 export default function(plugin, change) {
     const { value } = change;
-    const cursorBlock = value.document.getDescendant('_cursor_');
-    const offset = 2;
-
-    change.moveToRangeOf(cursorBlock).move(offset);
-
-    const position = plugin.utils.getPosition(change.value);
+    const position = plugin.utils.getPosition(value);
 
     expect(position.getWidth()).toEqual(3);
     expect(position.getHeight()).toEqual(3);
     expect(position.getRowIndex()).toEqual(1);
-    expect(position.getColumnIndex()).toEqual(1);
+    expect(position.getColumnIndex()).toEqual(0);
 
     return value;
 }
+
+export const input = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>
+                        <anchor />Col 0, Row 1<focus />
+                    </td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = undefined;
