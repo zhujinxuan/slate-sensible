400837d1a5b05e73f31f1ff73d50ebfa67344075
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 17:49:53 -0400

    Fix removeTable and removeRow

--- packages/slate-text-table/tests/remove-row-last-one/change.js
+++ packages/slate-text-table/tests/remove-row-last-one/change.js
@@ -1,7 +1,55 @@
-export default function(plugin, change) {
-    const { value } = change;
-    const cursorBlock = value.document.getDescendant('_cursor_');
-    change.moveToRangeOf(cursorBlock);
+/** @jsx h */
 
+import h from '../h';
+
+export default function(plugin, change) {
     return plugin.changes.removeRow(change);
 }
+
+export const input = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left">Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">Col 1, Row 1</td>
+                    <td textAlign="left">Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">
+                        <cursor />
+                        Col 0, Row 2
+                    </td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left">Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left">Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                    <td textAlign="left">Col 1, Row 1</td>
+                    <td textAlign="left">
+                        Col 2, Row 1<cursor />
+                    </td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
--- packages/slate-text-table/tests/remove-table/change.js
+++ packages/slate-text-table/tests/remove-table/change.js
@@ -1,7 +1,39 @@
-export default function(plugin, change) {
-    const { value } = change;
-    const cursorBlock = value.document.getDescendant('_cursor_');
-    change.moveToRangeOf(cursorBlock);
+/** @jsx h */
 
+import h from '../h';
+
+export default function(plugin, change) {
     return plugin.changes.removeTable(change);
 }
+
+export const input = (
+    <value>
+        <document>
+            <paragraph>Before</paragraph>
+            <table presetAlign={['left', 'left']}>
+                <tr>
+                    <td textAlign="left" />
+                    <td textAlign="left" />
+                </tr>
+                <tr>
+                    <td textAlign="left" />
+                    <td textAlign="left">
+                        <cursor />
+                    </td>
+                </tr>
+            </table>
+            <paragraph>After</paragraph>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <paragraph>Before</paragraph>
+            <paragraph>
+                <cursor />After
+            </paragraph>
+        </document>
+    </value>
+);
