b9cdc02aacd956e804cf6461b8b377693a20baa0
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 17:40:58 -0400

    Remove column for single column

---
+++ packages/slate-text-table/tests/remove-column-last-text/change.js
@@ -0,0 +1,48 @@
+/** @jsx h */
+
+import h from '../h';
+
+export default function(plugin, change) {
+    return plugin.changes.removeColumn(change);
+}
+
+export const input = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">
+                        <cursor />
+                        Col 0, Row 0
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 1</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">
+                        <cursor />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left" />
+                </tr>
+                <tr>
+                    <td textAlign="left" />
+                </tr>
+            </table>
+        </document>
+    </value>
+);
