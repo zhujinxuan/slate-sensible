98c21a33c75c282b069033e5e4a3c1ced2d53520
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 15:50:57 -0400

    Continuing the fix

--- packages/slate-text-table/tests/backspace-same-block/change.js
+++ packages/slate-text-table/tests/backspace-same-block/change.js
@@ -1,24 +1,46 @@
-import expect from 'expect';
+/** @jsx h */
+
+import h from '../h';
 
 export default function(plugin, change) {
-    const { value } = change;
-    const blockStart = value.document.getDescendant('anchor');
-    const blockEnd = value.document.getDescendant('anchor');
-
-    const withCursor = change
-        .collapseToStartOf(blockStart)
-        .extendToEndOf(blockEnd);
-
     const result = plugin.onKeyDown(
         {
             key: 'Backspace',
             preventDefault() {},
             stopPropagation() {}
         },
-        withCursor
+        change
     );
 
     expect(result).toBe(undefined);
 
     return change;
 }
+
+export const input = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>
+                        <anchor />Col 0, Row 1<focus />
+                    </td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = input;
