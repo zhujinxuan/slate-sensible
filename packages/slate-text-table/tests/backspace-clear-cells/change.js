168e4773bdb39fdc0ea720e7cde61f87a4e5586a
zhujinxuan <zhujinxuan@gmail.com>
2018-07-05 15:45:24 -0400

    Compat between yaml and hyperscript

--- packages/slate-text-table/tests/backspace-clear-cells/change.js
+++ packages/slate-text-table/tests/backspace-clear-cells/change.js
@@ -1,18 +1,68 @@
+/** @jsx h */
+
+import h from '../h';
+
 export default function(plugin, change) {
-    const { value } = change;
-    const blockStart = value.document.getDescendant('anchor');
-    const blockEnd = value.document.getDescendant('focus');
-
-    const withCursor = change
-        .collapseToStartOf(blockStart)
-        .extendToEndOf(blockEnd);
-
     return plugin.onKeyDown(
         {
             key: 'Backspace',
             preventDefault() {},
             stopPropagation() {}
         },
-        withCursor
+        change
     );
 }
+
+export const input = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>
+                        Col 0, Row 0<anchor />
+                    </td>
+                    <td>Col 1, Row 0</td>
+                    <td>
+                        <focus />Col 2, Row 0
+                    </td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 1</td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td />
+                    <td />
+                    <td>
+                        <cursor />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 1</td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
--- packages/slate-text-table/tests/backspace-collapsed/change.js
+++ packages/slate-text-table/tests/backspace-collapsed/change.js
@@ -1,17 +1,68 @@
+/** @jsx h */
+
+import h from '../h';
+
 export default function(plugin, change) {
-    const { value } = change;
-    const blockStart = value.document.getDescendant('anchor');
-
-    const withCursor = change.collapseToStartOf(blockStart);
-
     plugin.onKeyDown(
         {
             key: 'Backspace',
             preventDefault() {},
             stopPropagation() {}
         },
-        withCursor
+        change
     );
 
     return change;
 }
+
+export const input = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>
+                        <cursor />Col 0, Row 1
+                    </td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>
+                        <cursor />Col 0, Row 1
+                    </td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 2</td>
+                    <td>Col 1, Row 2</td>
+                    <td>Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
