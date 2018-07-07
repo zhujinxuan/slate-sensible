d4979f99ecaed0cb093fd8c684216029ca030e2b
zhujinxuan <zhujinxuan@gmail.com>
2018-07-06 22:50:49 -0400

    Fix schema for undo

--- packages/slate-text-table/tests/undo-remove-table/change.js
+++ packages/slate-text-table/tests/undo-remove-table/change.js
@@ -1,14 +1,60 @@
-import expect from 'expect';
+/** @jsx h */
+import h from '../h';
 
 export default function(plugin, change) {
-    const cursorBlock = change.value.document.getDescendant('_cursor_');
-    const initial = change.value.change({ save: false });
-    initial.moveToRangeOf(cursorBlock);
-    const toTest = initial.value.change();
-    toTest.call(plugin.changes.removeTable).undo();
-
-    // Back to previous cursor position
-    expect(toTest.value.startBlock.text).toEqual('Before');
-
-    return toTest;
+    plugin.changes.removeTable(change);
+    change.undo();
+    return change;
 }
+
+export const input = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left">Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left">
+                        <anchor />Col 2, Row 2<focus />
+                    </td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left">Col 2, Row 2</td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td textAlign="left">Col 0, Row 0</td>
+                    <td textAlign="left">Col 1, Row 0</td>
+                    <td textAlign="left">Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left">Col 2, Row 2</td>
+                </tr>
+                <tr>
+                    <td textAlign="left">Col 0, Row 2</td>
+                    <td textAlign="left">Col 1, Row 2</td>
+                    <td textAlign="left">
+                        Col 2, Row 2<cursor />
+                    </td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
