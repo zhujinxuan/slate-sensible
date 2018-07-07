07f2a48481e02126a9914b760584b93c5b37e735
zhujinxuan <zhujinxuan@gmail.com>
2018-07-06 22:59:38 -0400

    Add test for adjustrow without schema

---
+++ packages/slate-text-table/tests/insert-row-without-align/change.js
@@ -0,0 +1,54 @@
+/** @jsx h */
+
+import h from '../h';
+
+export default function(plugin, change) {
+    return plugin.changes.insertRow(change);
+}
+
+export const input = (
+    <value>
+        <document>
+            <table>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 1</td>
+                    <td>Col 1, Row 1</td>
+                    <td>
+                        <cursor />Col 2, Row 1
+                    </td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
+
+export const expected = (
+    <value>
+        <document>
+            <table presetAlign={['left', 'left', 'left']}>
+                <tr>
+                    <td>Col 0, Row 0</td>
+                    <td>Col 1, Row 0</td>
+                    <td>Col 2, Row 0</td>
+                </tr>
+                <tr>
+                    <td>Col 0, Row 1</td>
+                    <td>Col 1, Row 1</td>
+                    <td>Col 2, Row 1</td>
+                </tr>
+                <tr>
+                    <td />
+                    <td />
+                    <td>
+                        <cursor />
+                    </td>
+                </tr>
+            </table>
+        </document>
+    </value>
+);
